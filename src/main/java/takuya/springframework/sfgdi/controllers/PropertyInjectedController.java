756407359e6fcc31ce3a4971ddf8ba6c46868d4b
Takuya Shimada <gct3860@gmail.com>
2022-09-26 10:09:29 +0900

    add service qualifiers

--- src/main/java/takuya/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/takuya/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -1,12 +1,14 @@
 package takuya.springframework.sfgdi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import takuya.springframework.sfgdi.services.GreetingService;
 
 @Controller
 public class PropertyInjectedController {
 
+    @Qualifier("propertyInjectedGreetingService")
     @Autowired
     public GreetingService greetingService;
 
--- src/test/java/takuya/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/takuya/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -2,9 +2,7 @@ package takuya.springframework.sfgdi.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import takuya.springframework.sfgdi.services.GreetingServiceImpl;
-
-import static org.junit.jupiter.api.Assertions.*;
+import takuya.springframework.sfgdi.services.ConstructorGreetingService;
 
 class ConstructorInjectedControllerTest {
 
@@ -12,7 +10,7 @@ class ConstructorInjectedControllerTest {
 
     @BeforeEach
     void setUp() {
-        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+        controller = new ConstructorInjectedController(new ConstructorGreetingService());
     }
 
     @Test
